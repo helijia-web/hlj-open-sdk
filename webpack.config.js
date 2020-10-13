23aac93c88ed242ad3e2f59ddcf3a1510feca1ef
Youthink <hufy3651@foxmail.com>
2020-10-13 20:29:40 +0800

    change distpath config

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const { createConfig } = require('@hlj/webpack-config');
 
 module.exports = createConfig({
   digest: false,
+  distPath: 'dist',
   externals: {
     react: 'react',
     'react-dom': 'react-dom'
